3c63b6c9b149bf5f3d91c5f70768bc9dac6fd039
Jackeline <jackeline.delima17@gmail.com>
2025-09-20 12:53:33 -0300

    Adicionando linhas de codigo relacionadas a Horario.php
    Controller Horario (getters e setters e os método inserir(), consultar(), alterar() e desativar()).
    Vamos inicialmente documentar através de comentários, os retornos possíveis dentro de nossa classe, como fizemos em nossa classe anterior, no caso, a Sala. Em seguida, vamos codificar a parte de encapsulamento de nossa classe Horario, onde
    colocaremos os getters e setters, após isso iremos já fazer todos os métodos de nossa controller, a inserção, consulta, alteração e desativação.

--- application/controllers/Horario.php
+++ application/controllers/Horario.php
@@ -118,7 +118,248 @@ class Horario extends CI_Controller {
         echo json_encode($retorno);
     }
 
-    
+    public function consultar(){
+        //atributos para controlar o status de nosso método
+        $erros = [];
+        $sucesso = false;
+
+        try {
+            $json = file_get_contents('php://input');
+            $resultado = json_decode($json);
+            $lista = ["codigo"=>'0',"descricao"=>'0',"horaInicial"=>'0',"horaFinal"=>'0'];
+
+            if (verificarParam($resultado, $lista) !=1) {
+                //validar vindos de forma correta do front(helper)
+                $erros[]= ['codigo'=>99, 'msg'=>'Campos inexistentes ou incorretos no FrontEnd'];
+                
+            }else{
+                //validar campos quanto ao tipo de dado e tamanho(helper)
+                $retornoCodigo = validarDadosConsulta($resultado->codigo, 'int');
+                $retornoDescricao = validarDadosConsulta($resultado->descricao, 'string');
+                $retornoHoraInicial = validarDadosConsulta($resultado->horaInicial, 'hora');
+                $retornoHoraFinal = validarDadosConsulta($resultado->horaFinal, 'hora');
+
+                $retornoComparacaoHoras = compararDataHora($resultado-> horaInicial, $resultado->horaFinal, 'hora');
+
+                if ($retornoCodigo['codigoHelper'] != 0) {
+                    $erros[] = ['codigo'=> $retornoCodigo['codigoHelper'],
+                                'campo'=> 'Codigo',
+                                'msg' => $retornoCodigo['msg']];
+                }
+
+                if ($retornoDescricao['codigoHelper'] != 0) {
+                    $erros[] = ['codigo'=> $retornoDescricao['codigoHelper'],
+                                'campo'=> 'Descricao',
+                                'msg' => $retornoDescricao['msg']];
+                }
+
+                if ($retornoHoraInicial['codigoHelper'] != 0) {
+                    $erros[] = ['codigo'=> $retornoHoraInicial['codigoHelper'],
+                                'campo'=> 'Hora Inicial',
+                                'msg' => $retornoHoraInicial['msg']];
+                }
+
+                if ($retornoHoraFinal['codigoHelper'] != 0) {
+                    $erros[] = ['codigo'=> $retornoHoraFinal['codigoHelper'],
+                                'campo'=> 'Hora Final',
+                                'msg' => $retornoHoraFinal['msg']];
+                }
+
+                //validar se a hora inicial é maior que a hora final
+                if ($retornoComparacaoHoras['codigoHelper'] !=0) {
+                    $erros[] = ['codigo'=> $retornoComparacaoHoras['codigoHelper'],
+                                'campo'=> 'Hora inicial e Hora final',
+                                'msg'=> $retornoComparacaoHoras['msg']];
+                }
+
+                //se não encontrar erros
+                if (empty($erros)) {
+                    $this->setCodigo($resultado->codigo);
+                    $this->setDescricao($resultado->descricao);
+                    $this->setHoraInicial($resultado->horaInicial);
+                    $this->setHoraFinal($resultado->horaFinal);
+
+                    $this->load-<model('M_horario');
+                    $resBanco = $this->M_horario->consultar($this->getCodigo(),
+                                                            $this->getDescricao(),
+                                                            $this->getHoraInicial(),
+                                                            $this->getHoraFinal());
+                    if ($resBanco['codigo']==1) {
+                        $sucesso = true;
+                    }else{
+                        //captura erro do banco
+                        $erros[] = ['codigo'=>$resBanco['codigo'], 'msg'=> $resBanco['msg']];
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            $erros[] = ['codigo'=>0, 'msg'=>'Erro inesperado '.$e->getMessage()];
+        }
+
+        //monta um retorno unico
+        if ($sucesso == true) {
+            $retorno = ['sucesso'=> $sucesso, 'codigo'=>$resBanco['codigo'],
+                        'msg'=>$resBanco['msg'], 'dados'=>$resBanco['dados']];
+        }else {
+            $retorno = ['sucesso'=> $sucesso, 'erros'=> $erros];
+        }
+
+        //transforma o array em JSON
+        echo json_encode($retorno);
+    }
+
+    public function alterar(){
+        //atribuido para controlar o status de nosso método
+        $erros = [];
+        $sucesso = false;
+
+        try {
+            $json = file_get_contents('php://input');
+            $resultado = json_decode($json);
+            $lista = ["codigo"=>'0',"descricao"=>'0',"horaInicial"=>'0',"horaFinal"=>'0'];
+
+            if (verificarParam($resultado, $lista) !=1) {
+                //validar vindos de forma correta do front(helper)
+                $erros[]= ['codigo'=>99, 'msg'=>'Campos inexistentes ou incorretos no FrontEnd'];
+                
+            }else{
+                //pelo menos um dos tres parametros precisam ter dados para acontecer a atualizacao
+                if (trim($resultado->descricao)=='' && trim($resultado->horaInicial)=='' && trim($resultado->horaFinal)=='') {
+                    $erros[] = ['codigo'=>12, 'msg'=> 'Pelo menos um parametro precisar ser passado para atualização'];
+                }else{
+                    //validar campos quanto ao tipo de dado e tamanho(helper)
+                    $retornoCodigo = validarDados($resultado->codigo, 'int', true);
+                    $retornoDescricao = validarDados($resultado->descricao, 'string');
+                    $retornoHoraInicial = validarDados($resultado->horaInicial, 'hora');
+                    $retornoHoraFinal = validarDados($resultado->horaFinal, 'hora');
+
+                    $retornoComparacaoHoras = compararDataHora($resultado->horaInicial,$resultado->horaFinal, 'hora');
+                    
+                        if ($retornoCodigo['codigoHelper'] != 0) {
+                        $erros[] = ['codigo'=> $retornoCodigo['codigoHelper'],
+                                    'campo'=> 'Codigo',
+                                    'msg' => $retornoCodigo['msg']];
+                        }
+
+                    if ($retornoDescricao['codigoHelper'] != 0) {
+                        $erros[] = ['codigo'=> $retornoDescricao['codigoHelper'],
+                                    'campo'=> 'Descricao',
+                                    'msg' => $retornoDescricao['msg']];
+                    }
+
+                    if ($retornoHoraInicial['codigoHelper'] != 0) {
+                        $erros[] = ['codigo'=> $retornoHoraInicial['codigoHelper'],
+                                    'campo'=> 'Hora Inicial',
+                                    'msg' => $retornoHoraInicial['msg']];
+                    }
+
+                    if ($retornoHoraFinal['codigoHelper'] != 0) {
+                        $erros[] = ['codigo'=> $retornoHoraFinal['codigoHelper'],
+                                    'campo'=> 'Hora Final',
+                                    'msg' => $retornoHoraFinal['msg']];
+                    }
+
+                    //validar se a hora inicial é maior que a hora final
+                    if ($retornoComparacaoHoras['codigoHelper'] !=0) {
+                        $erros[] = ['codigo'=> $retornoComparacaoHoras['codigoHelper'],
+                                    'campo'=> 'Hora inicial e Hora final',
+                                    'msg'=> $retornoComparacaoHoras['msg']];
+                    }
+                    //se não encontrar erros
+                    if (empty($erros)) {
+                        $this->setCodigo($resultado->codigo);
+                        $this->setDescricao($resultado->descricao);
+                        $this->setHoraInicial($resultado->horaInicial);
+                        $this->setHoraFinal($resultado->horaFinal);
+
+                        $this->load-<model('M_horario');
+                        $resBanco = $this->M_horario->consultar($this->getCodigo(),
+                                                                $this->getDescricao(),
+                                                                $this->getHoraInicial(),
+                                                                $this->getHoraFinal());
+                        if ($resBanco['codigo']==1) {
+                            $sucesso = true;
+                        }else{
+                            //captura erro do banco
+                            $erros[] = ['codigo'=>$resBanco['codigo'], 'msg'=> $resBanco['msg']];
+                        }
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            $erros[] = ['codigo'=>0, 'msg'=>'Erro inesperado: '.$e->getMessage()];
+        }
+
+        // Monta retorno unico
+        if ($sucesso == true) {
+            $retorno = ['sucesso' => $sucesso, 'codigo'=> $resBanco['codigo'], 'msg'=> $resBanco['msg']];
+        } else {
+            $retorno = ['sucesso'=> $sucesso, 'erros'=>$erros];
+        }
+
+        //transforma o array em JSON
+        echo json_encode($retorno);
+    }
+
+    public function desativar(){
+        //atribuido para controlar o status de nosso método
+        $erros = [];
+        $sucesso = false;
+
+        try {
+            $json = file_get_contents('php://input');
+            $resultado = json_decode($json);
+            $lista = ["codigo"=>'0'];
+
+            if (verificarParam($resultado, $lista) !=1) {
+                //validar vindos de forma correta do front(helper)
+                $erros[]= ['codigo'=>99, 'msg'=>'Campos inexistentes ou incorretos no FrontEnd'];
+                
+            }else{
+                //validar codigo quanto ao tipo de dado e tamanho(helper)
+                $retornoCodigo = validarDados($resultado->codigo, 'int', true);
+
+                if ($retornoCodigo['codigoHelper'] != 0) {
+                    $erros[] = [
+                        'codigo' = $retornoCodigo['codigoHelper'],
+                        'campo' = 'Codigo',
+                        'msg' = $retornoCodigo['msg']
+                    ];
+                }
+
+                //se não encontrar erros
+                if (empty($erros)) {
+                    $this->setCodigo($resultado->codigo);
+                    $this->load->model('M_horario');
+                    $resBanco = $this->M_horario->desativar($this->getCodigo());
+
+                    if ($resBanco['codigo']==1){
+                        $sucesso = true;
+                    }else{
+                        //captura erro do banco
+                        $erros[] = [
+                            'codigo'=> $resBanco['codigo'],
+                            'msg'=> $resBanco['msg']
+                        ];
+                    }
+                }
+            }
+        } catch(Exception $e){
+            $erros[] = ['codigo'=> 0, 'msg'=> 'Erro inesperado: '.$e->getMessage()];
+        }
+
+        //monta retorno unico
+        if ($sucesso == true) {
+            $retorno = [
+                'sucesso' => $sucesso, 'codigo'=> $resBanco['codigo'], 'msg'=> $resBanco['msg']
+            ];
+        }else {
+            $retorno = ['sucesso'=> $sucesso, 'erros'=> $erros];
+        }
+
+        //transforma o array em JSON
+        echo json_encode($retorno);
+    }
 
 
 }
